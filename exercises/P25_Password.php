29344ae1d4d2a2acaa5d7f1c0fd33db05aacac9a
patricia <patricia.gsbcn@gmail.com>
2025-09-29 19:13:56 +0200

    ko25

--- exercises/P25_Password.php
+++ exercises/P25_Password.php
@@ -5,6 +5,12 @@ class P25_Password
     public function main(): void
     {
         // Write your code here
-       
+        echo "Password?\n";
+        $userPass = trim(fgets($GLOBALS['STDIN'] ?? STDIN));
+        if ($userPass === 'Wattlebird') {
+            echo "Off With you!";
+        } elseif ($userPass === 'Caput Draconis') {
+            echo "Welcome!";    
+        }
     }
 }
--- exercises/P26_Same.php
+++ exercises/P26_Same.php
@@ -5,6 +5,15 @@ class P26_Same
     public function main(): void
     {
         // Write your code here
+        echo "Enter the first string";
+        $inputFirts = trim(fgets($GLOBALS['STDIN'] ?? STDIN));
+        echo "Enter the second string";
+        $inputSecond = trim(fgets($GLOBALS['STDIN'] ?? STDIN));
+
+        if ($inputFirts === '') {
+
+        }
+        
        
     }
 }
