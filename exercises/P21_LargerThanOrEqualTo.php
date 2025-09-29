cf132b3b8ad21bfc9d9a50ee52082fd390fdedf8
patricia <patricia.gsbcn@gmail.com>
2025-09-29 17:37:57 +0200

    ok21

--- exercises/P21_LargerThanOrEqualTo.php
+++ exercises/P21_LargerThanOrEqualTo.php
@@ -10,19 +10,19 @@ class P21_LargerThanOrEqualTo
 
 
         // Get input from the user
-        $numberOne = (int)trim(fgets($GLOBALS['STDIN'] ?? STDIN));
+        $numberF = (int)trim(fgets($GLOBALS['STDIN'] ?? STDIN));
         
         // Prompt the user for input
         echo "Give the second number:\n";
         
         // Get input from the user
-        $numbertwo = (int)trim(fgets($GLOBALS['STDIN'] ?? STDIN));
+        $numberS = (int)trim(fgets($GLOBALS['STDIN'] ?? STDIN));
 
         // Check year value
-        if($numberOne > $numbertwo){
-            echo "Greater number is: ";
-        }elseif($numberOne < $numbertwo){
-            echo "Greater number is: ";
+        if($numberF> $numberS){
+            echo "Greater number is: ".$numberF;
+        }elseif($numberF < $numberS){
+            echo "Greater number is: ".$numberS;
         } else{
             echo "The numbers are equal!\n";
         }
