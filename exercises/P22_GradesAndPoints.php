dceab1fe40128bc48979b38ecff42677e610db59
patricia <patricia.gsbcn@gmail.com>
2025-09-29 18:18:57 +0200

    ok22

--- exercises/P22_GradesAndPoints.php
+++ exercises/P22_GradesAndPoints.php
@@ -7,12 +7,34 @@ class P22_GradesAndPoints
         // Write your code here
         echo "Give points[0-100]:\n";
         $points = (int)trim(fgets($GLOBALS['STDIN'] ?? STDIN));
-        if($points < 0 || $points < 50){       
-            echo $points."\nfailed";                
-        }elseif($points > 100){
-            echo $points."\nincredible!";
-        } else{
-            echo $points."\n";
+
+        switch (true) {
+            case $points < 0:
+                echo $points . "\nimpossible!";
+                break;
+            case $points >= 0 && $points < 50:
+                echo $points . "\nGrade: failed";
+                break;
+            case $points >= 50 && $points < 60:
+                echo "1\n";
+                break;
+            case $points >= 60 && $points < 70:
+                echo "2\n";
+                break;
+            case $points >= 70 && $points < 80:
+                echo "3\n";
+                break;
+            case $points >= 80 && $points < 90:
+                echo "4\n";
+                break;
+            case $points >= 90 && $points <= 100: 
+                echo "5\n";
+                break;
+            case $points > 100:
+                echo $points . "\nGrade: incredible!";
+                break;
+            default:
+            break;
         }
 
     }
--- exercises/P23_AbsoluteValue.php
+++ exercises/P23_AbsoluteValue.php
@@ -5,6 +5,15 @@ class P23_AbsoluteValue
     public function main(): void
     {
         // Write your code here
+        
+        $number = (int)trim(fgets($GLOBALS['STDIN'] ?? STDIN));
+        echo $number."\n";
+        if(is_integer($number)){       
+            echo $number."\n";                
+        }else{
+            $noInterger = $number * -1;
+            echo $noInterger."\n";
+        }
        
     }
 }
