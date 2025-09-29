09df4edbd0e725b6952b684860b1da49a656f56a
patricia <patricia.gsbcn@gmail.com>
2025-09-29 17:55:58 +0200

    KO22

--- exercises/P22_GradesAndPoints.php
+++ exercises/P22_GradesAndPoints.php
@@ -5,6 +5,15 @@ class P22_GradesAndPoints
     public function main(): void
     {
         // Write your code here
-        
+        echo "Give points[0-100]:\n";
+        $points = (int)trim(fgets($GLOBALS['STDIN'] ?? STDIN));
+        if($points < 0 || $points < 50){       
+            echo $points."\nfailed";                
+        }elseif($points > 100){
+            echo $points."\nincredible!";
+        } else{
+            echo $points."\n";
+        }
+
     }
 }
