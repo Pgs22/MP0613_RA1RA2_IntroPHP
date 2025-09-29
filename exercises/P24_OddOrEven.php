432df3fab007dfdc3a8b7bbdf8839bd78972803b
patricia <patricia.gsbcn@gmail.com>
2025-09-29 19:01:49 +0200

    KO24

--- exercises/P24_OddOrEven.php
+++ exercises/P24_OddOrEven.php
@@ -5,7 +5,16 @@ class P24_OddOrEven
     public function main(): void
     {
         // Write your code here
-        echo "Give points:\n";
+        echo "Give a number: \n";
+        $number = (int)trim(fgets($GLOBALS['STDIN'] ?? STDIN));
+
+        $resto = $number % 2;
+
+        if($resto === 0){
+            echo $number."\nNumber ".$number." is even.";
+        } else{
+            echo $number."\nNumber ".$number." is odd.";
+        }
       
     }
 }
