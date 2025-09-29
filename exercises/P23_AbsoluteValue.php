df48390b00c4827c4df1dbcc2daa46d45d2aeb35
patricia <patricia.gsbcn@gmail.com>
2025-09-29 18:50:47 +0200

    ok23

--- exercises/P23_AbsoluteValue.php
+++ exercises/P23_AbsoluteValue.php
@@ -4,16 +4,15 @@ class P23_AbsoluteValue
 {
     public function main(): void
     {
-        // Write your code here
-        
+        // Write your code here        
         $number = (int)trim(fgets($GLOBALS['STDIN'] ?? STDIN));
-        echo $number."\n";
-        if(is_integer($number)){       
-            echo $number."\n";                
+ 
+        if((int)$number > 0){
+            echo $number."\n";
         }else{
-            $noInterger = $number * -1;
-            echo $noInterger."\n";
-        }
-       
+            $numberMultipliet = $number * -1;
+            echo $numberMultipliet."\n";
+        } 
+
     }
 }
--- exercises/P24_OddOrEven.php
+++ exercises/P24_OddOrEven.php
@@ -5,6 +5,7 @@ class P24_OddOrEven
     public function main(): void
     {
         // Write your code here
+        echo "Give points:\n";
       
     }
 }
