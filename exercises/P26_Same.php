e4f7399f49593de496aa3b7f1af5aeb1a116c8a7
patricia <patricia.gsbcn@gmail.com>
2025-09-30 16:18:35 +0200

    ok26

--- exercises/P26_Same.php
+++ exercises/P26_Same.php
@@ -5,13 +5,15 @@ class P26_Same
     public function main(): void
     {
         // Write your code here
-        echo "Enter the first string";
+        echo "Enter the first string:\n";
         $inputFirts = trim(fgets($GLOBALS['STDIN'] ?? STDIN));
-        echo "Enter the second string";
+        echo "Enter the second string:\n";
         $inputSecond = trim(fgets($GLOBALS['STDIN'] ?? STDIN));
 
-        if ($inputFirts === '') {
-
+        if ($inputFirts === $inputSecond) {
+            echo "Same";
+        }else{
+            echo "Different";
         }
         
        
