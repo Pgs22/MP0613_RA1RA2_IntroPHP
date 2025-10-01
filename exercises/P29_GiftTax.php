9fd7f6c1030574b8e24e49154b8d6f8f33a4bc9e
patricia <patricia.gsbcn@gmail.com>
2025-10-01 15:22:28 +0200

    ok29

--- exercises/P29_GiftTax.php
+++ exercises/P29_GiftTax.php
@@ -6,12 +6,24 @@ class P29_GiftTax
     {
         // Write your code here
         echo "Value of the gift?\n";
-        $gift = trim(fgets($GLOBALS['STDIN'] ?? STDIN));
-        if($gift > 5000 && $gift < 25000){
+        $gift = (float)trim(fgets($GLOBALS['STDIN'] ?? STDIN));
+        if($gift < 5000){
+            echo "No tax!";
+        }elseif($gift >= 5000 && $gift < 25000){
             $taxLimit = 100 + ($gift-5000)* 0.08;
-            echo "The tax is ".$taxLimit;
-        }elseif($gift > 25000 && $gift < 50000){
-            
+            echo "Tax: ".$taxLimit;    
+        }elseif($gift >= 25000 && $gift < 55000){
+            $taxLimit = 1700 + ($gift-25000)* 0.10;
+            echo "Tax: ".$taxLimit;
+        }elseif($gift >= 55000 && $gift < 200000){
+            $taxLimit = 4700 + ($gift-55000)* 0.12;
+            echo "Tax: ".$taxLimit;
+        }elseif($gift >= 200000 && $gift < 1000000){
+            $taxLimit = 22100 + ($gift-200000)* 0.15;
+            echo "Tax: ".$taxLimit;
+        }elseif($gift >= 1000000){
+            $taxLimit = 142100 + ($gift-1000000)* 0.17;
+            echo "Tax: ".$taxLimit;
         }
     }
 }
