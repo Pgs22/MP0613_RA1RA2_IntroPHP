11bc74750a1e9f54ec6afdd0474d68daa1ceffd5
patricia <patricia.gsbcn@gmail.com>
2025-10-01 18:32:40 +0200

    ok31

--- exercises/P31_AreWeThereYet.php
+++ exercises/P31_AreWeThereYet.php
@@ -5,7 +5,15 @@ class P31_AreWeThereYet
     public function main(): void
     {
         // Write your code here
-        echo "Give a number:\n";
-        $input = (float)trim(fgets($GLOBALS['STDIN'] ?? STDIN));
-    }
+
+        while(true){
+            echo "Give a number:\n";
+            $input = (int)trim(fgets($GLOBALS['STDIN'] ?? STDIN));
+            if($input === 4){
+                break;
+            }else{
+                continue;
+            }    
+        }        
+    }   
 }
