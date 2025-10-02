d46d19aa28db469b3e40be43222ab9a8896a292c
patricia <patricia.gsbcn@gmail.com>
2025-10-02 16:08:45 +0200

    ko30

--- exercises/P33_NumberOfNumbers.php
+++ exercises/P33_NumberOfNumbers.php
@@ -5,6 +5,19 @@ class P33_NumberOfNumbers
     public function main(): void
     {
         // Write your code here
-        
+        while(true){                        
+            echo "Give a number:";
+            $input = (int)trim(fgets($GLOBALS['STDIN'] ?? STDIN));
+            if($input === 0){
+                break;
+            }
+            if($input < 0){
+                echo "Unsuitable number\n";
+                continue;
+            }elseif(($input % 2 === 0)){
+                $inputPar = $input * $input;
+                echo $inputPar;
+            }
+        }
     }
 }
