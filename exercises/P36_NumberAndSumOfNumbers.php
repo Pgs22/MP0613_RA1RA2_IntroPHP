2d19cf802a725818bb09a8ac7ad24e8b0ae719db
patricia <patricia.gsbcn@gmail.com>
2025-10-06 18:09:59 +0200

    ok36

--- exercises/P36_NumberAndSumOfNumbers.php
+++ exercises/P36_NumberAndSumOfNumbers.php
@@ -5,6 +5,22 @@ class P36_NumberAndSumOfNumbers
     public function main(): void
     {
         // Write your code here
-       
+        $suma = 0;
+        $count = 0;
+        while(true){                        
+            echo "Give a number:\n";
+            $input = (int)trim(fgets($GLOBALS['STDIN'] ?? STDIN));
+
+            if($input === 0){
+                break;
+            }
+            if($input !== 0){
+                $suma+= $input;
+                $count++;
+                continue;
+            }
+        }
+        echo "Number of numbers: ".$count."\n";   
+        echo "Sum of the numbers: ".$suma."\n";       
     }
 }
