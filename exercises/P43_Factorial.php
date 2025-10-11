cf4a86e82acea7bcde4374d1cf4cf7edd27f3c6b
patricia <patricia.gsbcn@gmail.com>
2025-10-11 13:25:56 +0200

    ok44

--- exercises/P43_Factorial.php
+++ exercises/P43_Factorial.php
@@ -5,6 +5,19 @@ class P43_Factorial
     public function main(): void
     {
         // Write your program here
+        $factorial = 1;
+        echo "Give a number:\n";
+        $n = (int)trim(fgets($GLOBALS['STDIN'] ?? STDIN));
         
+        for($i = 1; $i <= $n; $i++){
+            if($i === 1){
+                $factorial = $i;
+                 
+            }else{
+                $factorial = $factorial * $i;
+            }
+                    
+        }
+        echo "Factorial: ".$factorial . "\n";
     }
 }
