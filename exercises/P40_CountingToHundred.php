d47d239f7e622abb2989c253d1f17255d2e4993f
patricia <patricia.gsbcn@gmail.com>
2025-10-11 12:41:31 +0200

    ok40

--- exercises/P40_CountingToHundred.php
+++ exercises/P40_CountingToHundred.php
@@ -5,6 +5,12 @@ class P40_CountingToHundred
     public function main(): void
     {
         // Write your program here
-       
+        $input = (int)trim(fgets($GLOBALS['STDIN'] ?? STDIN));
+        if ($input <= 100) {
+            for($i = $input; $i <= 100; $i++){
+                echo $i."\n";
+            }                
+        }        
     }
+    
 }
