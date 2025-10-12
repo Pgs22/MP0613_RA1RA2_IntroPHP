09ea7ca3a102480fc538be1be32411c5eeb31b0c
patricia <patricia.gsbcn@gmail.com>
2025-10-12 10:15:27 +0200

    complete

--- exercises/P44_Swap.php
+++ exercises/P44_Swap.php
@@ -10,48 +10,24 @@ class P44_Swap
                 echo $value . "\n";
         }
 
-        echo "\n";
+        echo "\n\n";
 
         // Write your code here
-        echo "Give two indices to swap:\n";
-        $indicePos1 = -1; 
-        $indicePos2 = -1;
 
         
-        $inputUser = trim(fgets($GLOBALS['STDIN'] ?? STDIN)); 
-        //Usamos funciones: 
-            //array_values() 
-            // array_filter() 
-            // explode() 
-            // (trim) 
-        $indice2 = array_values(array_filter(explode(' ', $inputUser), 'trim'));
+        //Leemos los datos que introduce usuario y los guardamos en variables convirtiendo a enteros
+        $index1 = (int) trim(fgets(STDIN));
+        $index2 = (int) trim(fgets(STDIN));
 
-        //Usamos funció count() para saber contar los valores hay dentro del array, si hay dos o más añadimos valores a las variables
-        if (count($indice2) >= 2) {
-            $indicePos1 = (int)$indice2[0];
-            $indicePos2 = (int)$indice2[1];
-        } else {
-            return;
+        //Comprobamos que no estén vacías las posiciones del array a intercambiar
+        if (isset($array[$index1]) && isset($array[$index2])) {
+            // Intercambio de las posiciones
+            [$array[$index1], $array[$index2]] = [$array[$index2], $array[$index1]];
         }
-
-
-        //Para situarnos en la ultima posicion del array (Ya sabemos el tamaño del array, pero es una buena práctica por cambios futuros)
-        $maxIndex = count($array) - 1;
-        //Primero comprobamos que las posiciones insertadas por el usuario existan
-       if ($indicePos1 >= 0 && $indicePos1 <= $maxIndex && 
-            $indicePos2 >= 0 && $indicePos2 <= $maxIndex) {            
-            // Cambiamos los valores de posición            
-            // Guardamos el valor actual en una variable temporal
-            $temp = $array[$indicePos1];            
-            // Ahora asignamos el valor de la posición 1 en la posición 2
-            $array[$indicePos1] = $array[$indicePos2];            
-            // Ahora usamos el valor guardado y lo asignamos a la nueva posición
-            $array[$indicePos2] = $temp;
-        }
-
+ 
         //Imprimimos la array con las posiciones cambiadas
         foreach ($array as $value) {
-            echo $value . "\n";
+            echo $value;
         }
     }
 }
