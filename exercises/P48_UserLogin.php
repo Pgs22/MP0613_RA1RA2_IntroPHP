a7ebc059c4c99b527f471ebbb374b418b880640e
patricia <patricia.gsbcn@gmail.com>
2025-10-12 11:22:42 +0200

    completeSession

--- exercises/P48_UserLogin.php
+++ exercises/P48_UserLogin.php
@@ -1,5 +1,7 @@
 <?php
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 
 class P48_UserLogin {
     public function main(): void {
--- exercises/P49_SetLanguagePreference.php
+++ exercises/P49_SetLanguagePreference.php
@@ -1,5 +1,7 @@
 <?php
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 
 class P49_SetLanguagePreference {
     private array $allowedLanguages = ['en', 'es', 'fr', 'de'];
